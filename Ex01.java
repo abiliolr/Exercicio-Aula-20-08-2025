0f1dba510045c4a7471fb23a0a058d1a644c800e
abiliolr <48859628+abiliolr@users.noreply.github.com>
2025-08-27 12:47:30 -0300

    Update Ex01.java

--- Ex01.java
+++ Ex01.java
@@ -1,4 +1,5 @@
-public class Pessoa{
+//Classe Pessoa
+public abstract class Pessoa{
     private String nome;
     public String getNome(){
         return nome;
@@ -7,4 +8,58 @@ public class Pessoa{
         this.nome = nome;
     }
     public abstract String getTipoVoz();
+}
+
+//Classe Avo que herda da Classe Pessoa
+public class Avo extends Pessoa {
+    private int idade;
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String getTipoVoz() {
+        return "Voz grave e sábia";
+    }
+}
+
+//Classe Pai que herda da Classe Pessoa
+public class Pai extends Pessoa {
+    private String profissao;
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissão = profissao;
+    }
+
+    @Override
+    public String getTipoVoz() {
+        return "Voz firme e orientadora";
+    }
+}
+
+//Classe Filho que herda da Classe Pessoa
+public class Filho extends Pessoa {
+    private String brinquedoFavorito; // Atributo específico de Filho
+
+    public String getBrinquedoFavorito() {
+        return brinquedoFavorito;
+    }
+
+    public void setBrinquedoFavorito(String brinquedoFavorito) {
+        this.brinquedoFavorito = brinquedoFavorito;
+    }
+
+    @Override
+    public String getTipoVoz() {
+        return "Voz aguda e animada";
+    }
 }
